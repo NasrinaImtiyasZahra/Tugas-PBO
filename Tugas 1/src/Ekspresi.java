91906f614bb112ebe9a489dfebfa72c492e5da1f
Nasrina Imtiyas Zahra <nasrinaimtiyaszahra@gmail.com>
2025-03-02 21:27:52 +0800

    Add files via upload

---
+++ Tugas 1/src/Ekspresi.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg13020220239_nasrinaimtiyaszahra;
+
+/**
+ *
+ * @author mac
+ */
+/* pemakaian operator kondisional */
+public class Ekspresi {
+            public static void main(String[] args) {
+
+                // TODO Auto-generated method stub
+                /* KAMUS */
+                int x = 1;
+                int y = 2;
+                /* ALGORITMA */
+                System.out.print("x = "+ x + "\n");
+                System.out.print("y = "+ y + "\n");
+                System.out.print("hasil ekspresi = (x<y)?x:y = "+ ((x < y) ?
+                x : y)); /*Gunakan dalam kurung "(statemen dan kondisi)" untuk menyatakan
+                satu kesatuan pernyataan*/
+        }
+}
